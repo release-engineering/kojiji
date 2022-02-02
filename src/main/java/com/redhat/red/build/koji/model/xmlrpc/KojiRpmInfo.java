2af0d25fbb127b9041da0e98220a2b6c5ac64c08
Austin Brinkman <abrinkman@sonatype.com>
2022-02-02 14:46:08 -0600

    organize import

--- src/main/java/com/redhat/red/build/koji/model/xmlrpc/KojiRpmInfo.java
+++ src/main/java/com/redhat/red/build/koji/model/xmlrpc/KojiRpmInfo.java
@@ -23,11 +23,11 @@ import java.util.Date;
 import java.util.Map;
 import java.util.Objects;
 
-import com.redhat.red.build.koji.model.converter.AnythingToStringConverter;
 import org.commonjava.rwx.anno.Converter;
 import org.commonjava.rwx.anno.DataKey;
 import org.commonjava.rwx.anno.StructPart;
 
+import com.redhat.red.build.koji.model.converter.AnythingToStringConverter;
 import com.redhat.red.build.koji.model.converter.TimestampIntConverter;
 import com.redhat.red.build.koji.model.util.ExternalizableUtils;
 
